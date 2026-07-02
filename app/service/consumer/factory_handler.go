26d775779905fd730df1672a9b59f0ddcf48c852
walter <walter-eidi@hotmail.com>
2026-07-02 20:30:52 -0300

    continuado implementação de filter

--- app/service/consumer/factory_handler.go
+++ app/service/consumer/factory_handler.go
@@ -3,6 +3,3 @@ package consumer
 type FactoryHandler interface {
 	CreateStrategy(event *IntegrationEvent) (StrategyHandler, error)
 }
-type FilterFactory interface {
-	GetQueue(event *IntegrationEvent) (IntegrationEvent, error)
-}
